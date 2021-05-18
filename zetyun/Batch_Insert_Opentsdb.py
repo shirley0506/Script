53bb2f3d0fb60f7afa5542f086a595c297354608
shirleyxu <shirleyxu0506@163.com>
2021-05-18 18:05:21 +0800

    更新

--- zetyun/Batch_Insert_Opentsdb.py
+++ zetyun/Batch_Insert_Opentsdb.py
@@ -22,37 +22,34 @@ def assign_timestamp(month=dt.now().month, day=dt.now().day, hour=dt.now().hour,
     :return:
     '''
     assign_date = datetime.date(year=2021, month=month, day=day)
-    assign_time = datetime.time(hour=hour, minute=minute, second=random.randint(1, 61),
+    assign_time = datetime.time(hour=hour, minute=minute, second=random.randint(1, 59),
                                 microsecond=random.randint(1, 1000001))
-    a = int(datetime.datetime.combine(date=assign_date, time=assign_time).timestamp()*1000)
+    a = int(datetime.datetime.combine(date=assign_date, time=assign_time).timestamp() * 1000)
     return a
 
-
-
-IP = ['172.20.3.120', '172.20.3.121', '172.20.3.122']
 
 '''
 指标数据：实时写入
 '''
 
 
-def current():
-    s = requests.session()
+def current(metric_name, IP, s):
     ls = []
     while True:
         for ip in IP:
             json = {
-                "metric": "xuqq_tread_null_anomaly",
+                "metric": metric_name,
                 "timestamp": int(time.time() * 1000),
-                "value": random.randint(500, 601),
+                # "value": random.randint(1, 600),
+                "value": 60,
                 "tags": {
                     "host": ip
                 }
             }
             ls.append(json)
-        send_json(json, s)
-        with open('metric.txt', 'a') as f:
-            f.write(str(json))
+            with open('metric.txt', 'a') as f:
+                f.write(str(json) + '\n')
+        send_json(ls, s)
         time.sleep(60)
         ls = []
 
@@ -62,17 +59,17 @@ def current():
 '''
 
 
-def batch(a):
-    s = requests.Session()
+def batch(a, metric_name, s, value_min, value_max):
+    # s = requests.Session()
     ls = []
     for i in range(1, 61):
         for ip in IP:
             # 多序列指标
             json = {
-                "metric": "xuqq_specialDay",
+                "metric": metric_name,
                 "timestamp": a,
                 # "timestamp": int(time.time() * 1000),
-                "value": random.randint(500, 601),
+                "value": random.randint(value_min, value_max),
                 "tags": {
                     "host": ip
                 }
@@ -80,12 +77,34 @@ def batch(a):
             ls.append(json)
             with open('metric.txt', 'a') as f:
                 f.write(str(json) + '\n')
-        # send_json(ls, s)
+        send_json(ls, s)
         i += 1
         a -= 60000
 
 
 if __name__ == "__main__":
-    variable = {'month': 5, 'day': 14, 'hour': 10, 'minute': 30}
-    past_timestamp = assign_timestamp(**variable)
-    batch(past_timestamp)
+    s = requests.session()
+    variable_parameter = []
+    metric_name = 'xuqq_specialDay_9'
+    IP = ['172.20.3.120', '172.20.3.121', '172.20.3.122']
+    # 批量写指标数据
+    # last_day = 7
+    # early_day = 7
+    # b = random.randint(1, 100)
+    # print(str(last_day) + "至" + str(early_day) + "日最大值：" + str(b+100) + ",最小值：" + str(b))
+    # for i in range(early_day, last_day+1):
+    #     variable_parameter.append([])
+    #     # b = random.randint(1, 100)
+    #     variable_parameter[i - early_day].append(i)
+    #     variable_parameter[i - early_day].append(b)
+    #     variable_parameter[i - early_day].append(b + 100)
+    # for i in range(len(variable_parameter)):
+    #     day = variable_parameter[i][0]
+    #     value_min = variable_parameter[i][1]
+    #     value_max = variable_parameter[i][2]
+    #     variable = {'month': 5, 'day': day, 'hour': 10, 'minute': 0}
+    #     past_timestamp = assign_timestamp(**variable)
+    #     batch(past_timestamp, metric_name, s, value_min, value_max)
+    # # 写实时日志
+    current(metric_name, IP, s)
+
